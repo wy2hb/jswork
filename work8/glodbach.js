c5cf0ec7267fbab4ebe7d3d6d6f3e4be60657c05
wy2hb <1581631670@qq.com>
2019-10-26 23:43:05 +0800

    work8

--- work8/glodbach.js
+++ work8/glodbach.js
@@ -1,24 +1,29 @@
-    var goldbach = parseInt(prompt("odd"))
-        isTrue(goldbach)
-        function factorial(a){
-            var sum=0;
-            for(var i=1;i<=a;i++){
-                if(a % i ==0){
-                    sum++;
-                }
-            }
-            if(sum==2){
-                return true;
-            }else{
-                return false;
-
-            }
-        }
-        function isTrue(a){
-            for(var i=2;i<a-2;i++){
-                var j = a -i;
-                if(factorial(i)&&factorial(j)){
-                    return 
-                }
+function goldbach(){
+    var num = document.getElementById('odd').value
+    if(num%2!==0){
+        alert("请输入一个大于2的偶数")
+    }
+    let goldbach = document.getElementById('goldbach')
+    var arr=[]
+    var a = 0
+    for(var i=2;i<=num;i++){
+        a=0
+        for(var j=2;j<i;j++){
+            if(i%j==0){
+                a++
             }
         }
+        if(a==0){
+            arr.push(i)
+        }
+    }
+    var str = ''
+    for(let i=0;i<(arr.length)/2;i++){
+        for(let j=0;j<arr.length;j++){
+            if((arr[i]+arr[j])===Number(num)){
+                str +='<div>'+num+"可以拆分为两个质数"+arr[i]+"与"+arr[j]+"的和"+'</div>'
+            }
+        }
+    }
+        goldbach.innerHTML = str
+    }
