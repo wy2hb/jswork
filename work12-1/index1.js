e0a208d473adf11633b151df07c289fe41333b67
wy2hb <1537412137@qq.com>
2020-12-02 20:46:45 +0800

    remove directory from remote repository

--- work12-1/index1.js
+++
@@ -1,8 +0,0 @@
-let year = new Date().getFullYear()
-document.getElementById("cal").innerHTML = calendar(year)
-function setCalendar(year){
-    document.getElementById("preyear") = year - 1
-    document.getElementById("curyear") = year
-    document.getElementById("nextyear") = year + 1
-    document.getElementById("cal") = calendar(year)
-    }
