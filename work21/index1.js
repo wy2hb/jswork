8691c408ebaf8c6cad398a25b1a8692fa16bc3df
wy2hb <1581631670@qq.com>
2020-12-02 21:02:31 +0800

    work16

--- work21/index1.js
+++
@@ -1,6 +0,0 @@
-$("input:last").click(function(){
-    $.post("/ajax",$('form').serialize(),function(res){
-        result = res.reverse().map((val)=>{return JSON.stringify(val)+"<br>"})
-        $("#result").empty().append($("<div></div>").html(result))
-    })
-})
